0a30709db6fe5c3747ee8dff9091850ec81ddc54
Sumit Sasidharan <sumit.onorall@gmail.com>
2022-05-12 19:14:01 +0530

    updated client

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
-import './App.css';
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Route } from 'react-router-dom';
+import "./App.css";
+import Chatpage from "./pages/Chatpage";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <div className="App">
-      <h1>Chat App</h1>
+      <Route path="/" component={Homepage} exact />
+      <Route path="/chats" component={Chatpage} />
     </div>
   );
 }
